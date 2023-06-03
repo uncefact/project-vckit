228765692336e4d73cd03c8f298de1aa46b9ba00
Nam Hoang <hoangxuannam160493@gmail.com>
2023-06-03 14:53:17 +0700

    feat: implement WebRenderingTemplate2022 renderer provider
    Signed-off-by: Nam Hoang <hoangxuannam160493@gmail.com>

---
+++ packages/renderer/src/providers/web-rendering-template-2022.ts
@@ -0,0 +1,25 @@
+import { IRendererProvider, RenderDocument } from '@vckit/core-types';
+import handlebars from 'handlebars';
+
+/**
+ * WebRenderingTemplate2022 class implements the IRendererProvider interface for rendering verifiable credentials using Handlebars templates.
+ * @public
+ */
+export class WebRenderingTemplate2022 implements IRendererProvider {
+  async renderCredential(
+    template: string,
+    document: RenderDocument
+  ): Promise<string> {
+    // Check if the template is empty or contains only whitespace
+    if (!template.trim()) {
+      return '';
+    }
+
+    const compiledTemplate = handlebars.compile(template);
+
+    // Render the template with the document data
+    const renderedContent = compiledTemplate(document);
+
+    return renderedContent;
+  }
+}
