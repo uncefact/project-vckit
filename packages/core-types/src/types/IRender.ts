47730ba0607617659da4cedccbc23f825be86038
Nam Hoang <hoangxuannam160493@gmail.com>
2023-06-03 14:49:04 +0700

    feat: implement renderer plugin
    Signed-off-by: Nam Hoang <hoangxuannam160493@gmail.com>

---
+++ packages/core-types/src/types/IRender.ts
@@ -0,0 +1,54 @@
+import { IPluginMethodMap, IAgentContext } from './IAgent';
+import { IDIDManager } from './IDIDManager';
+import { IDataStore } from './IDataStore';
+import { IKeyManager } from './IKeyManager';
+import { IResolver } from './IResolver';
+import { UnsignedCredential, VerifiableCredential } from './vc-data-model';
+
+/**
+ * Arguments for rendering a verifiable credential.
+ * @public
+ */
+export interface IRenderCredentialArgs {
+  credential: VerifiableCredential | UnsignedCredential;
+}
+
+/**
+ * Context required for the renderer plugin.
+ * @public
+ */
+export type IRendererContext = IAgentContext<
+  IResolver &
+    Pick<IDIDManager, 'didManagerGet' | 'didManagerFind'> &
+    Pick<
+      IDataStore,
+      | 'dataStoreSaveVerifiablePresentation'
+      | 'dataStoreSaveVerifiableCredential'
+    > &
+    Pick<IKeyManager, 'keyManagerGet' | 'keyManagerSign'>
+>;
+
+/**
+ * Result of rendering a verifiable credential.
+ * @beta
+ */
+export interface IRenderResult {
+  documents: string[];
+}
+
+/**
+ * Plugin interface for the renderer.
+ * @public
+ */
+export interface IRenderer extends IPluginMethodMap {
+  /**
+   * Renders a verifiable credential.
+   * @param args - The rendering arguments.
+   * @param context - The renderer context.
+   * @returns A promise that resolves to the rendering result.
+   */
+  renderCredential(
+    args: IRenderCredentialArgs,
+    context: IRendererContext
+  ): Promise<IRenderResult>;
+}
---
+++ packages/renderer/README.md
@@ -0,0 +1,3 @@
+# Renderer Plugin
+
+The Renderer Plugin is a module that provides rendering capabilities for verifiable credentials. It allows you to render verifiable credentials using different render methods, such as HTML templates.
