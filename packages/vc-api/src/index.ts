0d33153f9449b1ddfd1aedc045b4861e92acbd55
Nam Hoang <hoangxuannam160493@gmail.com>
2023-06-22 17:52:31 +0700

    docs: add api docs for vc api
    Signed-off-by: Nam Hoang <hoangxuannam160493@gmail.com>

--- packages/vc-api/src/index.ts
+++ packages/vc-api/src/index.ts
@@ -1,3 +1,5 @@
 export { VerifierRouter, VerifierRouterOptions } from './verifier-router.js'
 export { IssuerRouter, IssuerRouterOptions } from './issuer-router.js';
 export { HolderRouter } from './holder-router.js';
+export { VCApiSchemaRouter } from './vc-api-schema-router.js';
+export { VCApiDocsRouter } from './vc-api-docs-router.js';
