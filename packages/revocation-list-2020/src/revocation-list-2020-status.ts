f60f6cfb90f201498604362dced9be10d2344dc1
Nam Hoang <hoangxuannam160493@gmail.com>
2023-07-20 15:39:05 +0700

    feat: implement checkStatus function for RevocationList2020Status
    Signed-off-by: Nam Hoang <hoangxuannam160493@gmail.com>

---
+++ packages/revocation-list-2020/src/revocation-list-2020-status.ts
@@ -0,0 +1,55 @@
+import { CredentialJwtOrJSON, CredentialStatus } from 'credential-status';
+import { checkStatus as transmuteCheckStatus } from '@transmute/vc-status-rl-2020';
+import { Ed25519Signature2018 } from '@transmute/ed25519-signature-2018';
+import { JsonWebSignature } from '@transmute/json-web-signature';
+import { contexts } from '@transmute/did-context/index.js';
+import { DocumentLoader } from '@transmute/vc-status-rl-2020/dist/types';
+import * as didKey from '@transmute/did-key.js';
+import * as didWeb from '@transmute/did-web';
+
+const resolve = async (did: string) => {
+  if (did.startsWith('did:key')) {
+    const { didDocument } = await didKey.resolve(did.split('#')[0], {
+      accept: 'application/did+json',
+    });
+    return didDocument;
+  }
+  if (did.startsWith('did:web')) {
+    return didWeb.resolve(did);
+  }
+  throw new Error('Unsupported did method');
+};
+
+const documentLoader: DocumentLoader = async (iri: string) => {
+  if (iri) {
+    if (contexts.get(iri)) {
+      return { documentUrl: iri, document: contexts.get(iri) };
+    }
+
+    if (iri.startsWith('did:')) {
+      const didDocument = await resolve(iri);
+      return { documentUrl: iri, document: didDocument };
+    }
+
+    if (iri.startsWith('http')) {
+      const document = await fetch(iri).then((res) => res.json());
+      return { documentUrl: iri, document };
+    }
+  }
+
+  const message = 'Unsupported iri: ' + iri;
+  throw new Error(message);
+};
+
+export async function checkStatus(credential: CredentialJwtOrJSON) {
+  const result = await transmuteCheckStatus({
+    credential,
+    documentLoader,
+    suite: [new Ed25519Signature2018(), new JsonWebSignature()],
+  });
+
+  const status: CredentialStatus = {
+    revoked: !result.verified,
+  };
+  return status;
+}
