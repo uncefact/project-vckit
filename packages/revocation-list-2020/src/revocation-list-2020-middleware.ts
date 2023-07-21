4f3523bef3f512743cfa63555eafa9233e6a67e7
Nam Hoang <hoangxuannam160493@gmail.com>
2023-07-21 10:00:57 +0700

    fix: add the revocation list 2020 context to the vc
    Signed-off-by: Nam Hoang <hoangxuannam160493@gmail.com>

--- packages/revocation-list-2020/src/revocation-list-2020-middleware.ts
+++ packages/revocation-list-2020/src/revocation-list-2020-middleware.ts
@@ -1,8 +1,5 @@
-import {
-  IAgent,
-  IEncrypteAndStoreDataResult,
-  RequestWithAgent,
-} from '@vckit/core-types';
+import { RequestWithAgent } from '@vckit/core-types';
+import { asArray, processEntryToArray } from '@veramo/utils';
 import {
   NextFunction,
   Request,
@@ -11,9 +8,7 @@ import {
   urlencoded,
   json,
 } from 'express';
-import interceptor from 'express-interceptor';
-import { RevocationStatus2020 } from './revocation-list-2020';
-// import { RequestWithAgent } from './encrypted-store-router.js';
+const VC_REVOCATION_LIST_2020 = 'https://w3id.org/vc-revocation-list-2020/v1';
 
 /**
  *
@@ -48,6 +43,21 @@ export function revocationList2020(args: {
           revocationVCIssuer: args.revocationVCIssuer,
           req,
         });
+
+        req.body.credential['@context'] = asArray<string>(
+          req.body.credential['@context']
+        );
+
+        if (
+          !req.body.credential['@context'].find(
+            (context: string) => context === VC_REVOCATION_LIST_2020
+          )
+        ) {
+          req.body.credential['@context'] = [
+            ...req.body.credential['@context'],
+            VC_REVOCATION_LIST_2020,
+          ];
+        }
 
         req.body.credential.credentialStatus = {
           id: `${args.revocationListPath}${revocationData.revocationListFullUrlPath}`,
