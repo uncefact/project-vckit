84bf35150fa11def0885464c1f5520e35f4c654f
Nam Hoang <hoangxuannam160493@gmail.com>
2024-02-07 06:12:17 +0700

    refactor: cache the context to reduce fetching
    Signed-off-by: Nam Hoang <hoangxuannam160493@gmail.com>

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -2,3 +2,4 @@ import { Renderer } from './renderer.js';
 import { WebRenderingTemplate2022 } from './providers/web-rendering-template-2022.js';
 
 export { Renderer, WebRenderingTemplate2022 };
+export { RenderDefaultContexts } from './render-default-contexts.js';
---
+++ packages/revocation-list-2020/src/revocaltion-list-default-contexts.ts
@@ -0,0 +1,12 @@
+import w3id_org_vc_revocation_list_2020_v1 from './contexts/w3id.org_vc-revocation-list-2020_v1.json' assert { type: 'json' };
+
+/**
+ * @beta
+ * Provides default contexts for revocation list 2020
+ */
+export const RevocationListDefaultContexts = new Map([
+  [
+    'https://w3id.org/vc-revocation-list-2020/v1',
+    w3id_org_vc_revocation_list_2020_v1,
+  ],
+]);
