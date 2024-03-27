d11b01d91684334dbfac003d282506b024f31f3b
ducpm511 <117595496+ducpm511@users.noreply.github.com>
2024-03-27 15:33:33 +0700

    refactor: clean up the initialized project by removing sample file from docusaurus

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'VCKit Documentation',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
