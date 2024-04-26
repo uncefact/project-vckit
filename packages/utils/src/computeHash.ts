61ed51f0ca62a1d80e0a6603e1e60d5c14a7bee6
Yen Le <ldhyen99@gmail.com>
2024-04-27 00:06:28 +0700

    feat: add compute hash utils

---
+++ packages/utils/src/computeHash.ts
@@ -0,0 +1,20 @@
+import crypto from 'crypto';
+import multihashes from 'multihashes';
+
+/**
+ * Computes a hash of the template.
+ * @param content - The template need to hash.
+ * @returns The hash of the template as a multibase encoded string.
+ */
+export const computeHash = (content: string): string => {
+  // Create a SHA-256 hash of the template
+  const hash = crypto.createHash('sha256').update(content).digest();
+
+  // Create a multihash with SHA-256 algorithm and 256 bits output length
+  const multihash = multihashes.encode(hash, 'sha2-256');
+
+  // Encode the multihash using multibase with the prefix 'z'
+  const multibaseEncodedHash = multihashes.toB58String(multihash);
+
+  return multibaseEncodedHash;
+};
