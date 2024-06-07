cce6eb633e346c967e2622724f86078bebcb56c4
Duc Minh Pham <ducpm0511@gmail.com>
2024-06-07 10:23:35 +0700

    chore: added more test case to increase the coverage

--- packages/renderer/__tests__/providers/render-template-2024.test.ts
+++ packages/renderer/__tests__/providers/render-template-2024.test.ts
@@ -116,5 +116,181 @@ describe('RenderTemplate2024', () => {
     );
   });
 
-  // Add more tests here for different scenarios
+  it('should throw an error if the template is not a valid handlebars template', async () => {
+    const document = { name: 'John Doe' };
+    const data = {
+      'https://schema.org/encodingFormat': [
+        {
+          '@value': 'text/html',
+        },
+      ],
+      'https://www.w3.org/2018/credentials#renderMethod#template': [
+        {
+          '@value':
+            '<p>{{name}</p>', // Invalid handlebars template missing closing curly brace
+        },
+      ],
+      'https://www.w3.org/2018/credentials#renderMethod#url': [
+        {
+          '@value': '',
+        },
+      ],
+    };
+    await expect(async () => {
+      await renderer.renderCredential({
+        data,
+        document,
+      });
+    }).rejects.toThrow();
+  });
+
+  it('should return an error message if the template is not a string', async () => {
+    const document = { name: 'John Doe' };
+    const data = {
+      'https://schema.org/encodingFormat': [
+        {
+          '@value': 'text/html',
+        },
+      ],
+      'https://www.w3.org/2018/credentials#renderMethod#template': [
+        {
+          '@value': 123, // Invalid template value, should be a string
+        },
+      ],
+      'https://www.w3.org/2018/credentials#renderMethod#url': [
+        {
+          '@value': '',
+        },
+      ],
+    };
+    await expect(async () => {
+      await renderer.renderCredential({
+        data,
+        document,
+      });
+    }).rejects.toThrow();
+  });
+  
+  it('should return an error message if the template is not provided and there is no default template', async () => {
+    const data = {
+      'https://w3id.org/security#digestMultibase': [
+        {
+          '@type': 'https://w3id.org/security#multibase',
+          '@value': '',
+        },
+      ],
+      'https://www.w3.org/2018/credentials#renderMethod#mediaQuery': [
+        {
+          '@value':
+            '',
+        },
+      ],
+      'https://schema.org/encodingFormat': [
+        {
+          '@value': 'text/html',
+        },
+      ],
+      'https://www.w3.org/2018/credentials#renderMethod#url': [
+        {
+          '@value': '',
+        },
+      ],
+    };
+    const rendererWithoutDefaultTemplate = new RenderTemplate2024();
+    const result = await rendererWithoutDefaultTemplate.renderCredential({ data, document: {} });
+    expect(result).toStrictEqual(
+      {"renderedTemplate": "Error: No template or url provided"}
+    );
+  });
+
+  it('should render the template with the credential data and media query', async () => {
+    const document = { name: 'John Doe' };
+    const data = {
+      'https://schema.org/encodingFormat': [
+        {
+          '@value': 'text/html',
+        },
+      ],
+      'https://www.w3.org/2018/credentials#renderMethod#template': [
+        {
+          '@value':
+            '<p>{{name}}</p>',
+        },
+      ],
+      'https://www.w3.org/2018/credentials#renderMethod#url': [
+        {
+          '@value': '',
+        },
+      ],
+      'https://www.w3.org/2018/credentials#renderMethod#mediaQuery': [
+        {
+          '@value':
+            '@media (min-width: 1024px) {\n  .title {\n    font-weight: bold;\n    color: #223675;\n  }\n}',
+        },
+      ],
+    };
+
+    const renderedContent = await renderer.renderCredential({
+      data,
+      document,
+    });
+
+    expect(renderedContent).toEqual({"renderedTemplate": "<style>@media (min-width: 1024px) {\n  .title {\n    font-weight: bold;\n    color: #223675;\n  }\n}</style><p>John Doe</p>"});
+  });
+
+  it('should return an error message if the mediaType is unsupported', async () => {
+    const document = { name: 'John Doe' };
+    const data = {
+      'https://schema.org/encodingFormat': [
+        {
+          '@value': 'application/json',
+        },
+      ],
+      'https://www.w3.org/2018/credentials#renderMethod#template': [
+        {
+          '@value':
+            '<p>{{name}}</p>',
+        },
+      ],
+      'https://www.w3.org/2018/credentials#renderMethod#url': [
+        {
+          '@value': '',
+        },
+      ],
+      'https://www.w3.org/2018/credentials#renderMethod#mediaType': [
+        {
+          '@value': 'application/json', // Unsupported mediaType
+        },
+      ],
+    };
+    const result = await renderer.renderCredential({
+      data,
+      document,
+    });
+    expect(result).toStrictEqual(
+      {"renderedTemplate": "Error: Unsupported media type"}
+    );
+  });
+
+  it('should return the template if it failed to fetch template from url', async () => {
+    const document = { name: 'John Doe' };
+    const data = {
+      'https://schema.org/encodingFormat': [
+        {
+          '@value': 'text/html',
+        },
+      ],
+      'https://www.w3.org/2018/credentials#renderMethod#template': [
+        {
+          '@value':
+            '<p>{{name}}</p>',
+        },
+      ]
+    };
+    const renderedContent = await renderer.renderCredential({
+      data,
+      document,
+    });
+    expect(renderedContent).toEqual({"renderedTemplate": "<p>John Doe</p>"});
+  });
 });
--- packages/renderer/__tests__/renderer.test.ts
+++ packages/renderer/__tests__/renderer.test.ts
@@ -9,10 +9,22 @@ import {
   IRenderedResult
 } from '@vckit/core-types';
 import universityDegreeCredential from '../fixtures/university-degree-credential.json';
+import universityDegreeCredentialTemplate2024 from '../fixtures/univerisity-degree-credential-template-2024.json';
+import { RenderTemplate2024 } from '../src/providers/render-template-2024';
+// import { extractRenderMethods } from '../src/renderer';
 
 describe('Renderer', () => {
   // Mock renderer provider
   const MockWebRenderingTemplate2022: IRendererProvider = {
+    async renderCredential({data: any, document: RenderDocument}): Promise<IRenderedResult> {
+      // Mock implementation
+      return {
+        renderedTemplate: 'Rendered credential successfully'
+      };
+    },
+  };
+
+  const MockRenderTemplate2024: IRendererProvider = {
     async renderCredential({data: any, document: RenderDocument}): Promise<IRenderedResult> {
       // Mock implementation
       return {
@@ -33,10 +45,17 @@ describe('Renderer', () => {
   const rendererMockProvider = new Renderer({
     providers: {
       WebRenderingTemplate2022: MockWebRenderingTemplate2022,
+      RenderTemplate2024: MockRenderTemplate2024
     },
     defaultProvider: 'WebRenderingTemplate2022',
   });
 
+  const renderer2024Provider = new Renderer({
+    providers: {
+      RenderTemplate2024: MockRenderTemplate2024
+    },
+    defaultProvider: 'RenderTemplate2024',
+  });
   // Create an instance of Renderer with no default provider
   const rendererNoDefault = new Renderer({
     providers: {
@@ -123,77 +142,6 @@ describe('Renderer', () => {
     ).rejects.toThrow('Render method not found in the verifiable credential');
   });
 
-  //these 2 cases below are out of date, the logic of extracting data from the credential has been moved to the provider
-
-  // it('should throw an error if the verifiable credential contains an invalid render method', async () => {
-  //   // Mock data
-  //   const args: IRenderCredentialArgs = {
-  //     credential: {
-  //       '@context': [
-  //         'https://www.w3.org/2018/credentials/v1',
-  //         'https://www.w3.org/2018/credentials/examples/v1',
-  //         {
-  //           render: 'https://www.w3.org/2018/credentials#invalidRenderMethod',
-  //         },
-  //       ],
-  //       id: 'http://example.edu/credentials/3732',
-  //       type: ['VerifiableCredential', 'UniversityDegreeCredential'],
-  //       issuer: 'https://example.edu/issuers/565049',
-  //       issuanceDate: '2010-01-01T00:00:00Z',
-  //       credentialSubject: {
-  //         id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
-  //       },
-  //       render: [
-  //         {
-  //           '@id':
-  //             '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="100">\n  <rect width="100%" height="100%" style="fill:rgb(0,0,255);stroke-width:2;stroke:rgb(0,0,0)" />\n  <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" style="font-size:16px">Jane Smith</text>\n</svg>',
-  //           '@type': 'SvgRenderingHint2022',
-  //         },
-  //       ],
-  //     },
-  //   };
-  //   const context = {};
-
-  //   // Call the renderCredential method
-  //   await expect(
-  //     rendererMockProvider.renderCredential(args, context as IRendererContext),
-  //   ).rejects.toThrow('Render method must have template and @type.');
-  // });
-
-  // it('should skip render methods without @type or template', async () => {
-  //   // Mock data
-  //   const args: IRenderCredentialArgs = {
-  //     credential: {
-  //       ...universityDegreeCredential,
-  //       render: [
-  //         {
-  //           template:
-  //             '<div style="width:300px; height:100px; border: 2px solid black; text-align:center">\n  <div>\n    This {{credentialSubject.degree.name}} is conferred to\n  </div>\n  <strong style="font-size: 16px">\n    {{credentialSubject.name}}\n  </strong>\n  <div>\n    by {{credentialSubject.degree.institution}}.\n  </div>\n</div>',
-  //           '@type': 'SvgRenderingHint2022',
-  //         },
-  //         {
-  //           template:
-  //             '<div style="width:300px; height:100px; border: 2px solid black; text-align:center">\n  <div>\n    This {{credentialSubject.degree.name}} is conferred to\n  </div>\n  <strong style="font-size: 16px">\n    {{credentialSubject.name}}\n  </strong>\n  <div>\n    by {{credentialSubject.degree.institution}}.\n  </div>\n</div>',
-  //         },
-  //         {
-  //           '@type': 'SvgRenderingHint2022',
-  //         },
-  //       ],
-  //     },
-  //   };
-  //   const context = {};
-
-  //   // Call the renderCredential method
-  //   const result: IRenderResult = await rendererMockProvider.renderCredential(
-  //     args,
-  //     context as IRendererContext
-  //   );
-  //   expect(result.documents.length).toBe(1);
-  //   expect(result.documents[0]).toEqual(
-  //     'UmVuZGVyZWQgY3JlZGVudGlhbCBzdWNjZXNzZnVsbHk='
-  //   );
-  // });
-
   it('should throw an error with invalid @type and non-default provider', async () => {
     // Mock data
     const args: IRenderCredentialArgs = {
@@ -225,4 +173,29 @@ describe('Renderer', () => {
       rendererNoDefault.renderCredential(args, context as IRendererContext)
     ).rejects.toThrow('Renderer provider invalid-render-type not found');
   });
+
+  it('should render a verifiable credential using the RenderTemplate2024 render method', async () => {
+    // Mock data
+    const args: IRenderCredentialArgs = {
+      credential: {
+        ...universityDegreeCredentialTemplate2024,
+      },
+    };
+    const context = {};
+    // Call the renderCredential method
+    const result: IRenderResult = await renderer2024Provider.renderCredential(
+      args,
+      context as IRendererContext
+    );
+    // Verify the result
+    expect(result.documents).toEqual([
+      {
+        "renderedTemplate": "UmVuZGVyZWQgY3JlZGVudGlhbCBzdWNjZXNzZnVsbHk=",
+        "type": "RenderTemplate2024"
+      }
+    ]);
+  });
+  
+  
+  
 });
