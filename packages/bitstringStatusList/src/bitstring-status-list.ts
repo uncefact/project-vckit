6477aadd4c9f1ba3217d6a6869cada83eadd371b
Huy Nguyen <huy.nguyenkim.h@gmail.com>
2024-07-24 14:12:55 +0700

    chore: remove unnecessary code of the bitstring-status-list

--- packages/bitstringStatusList/src/bitstring-status-list.ts
+++ packages/bitstringStatusList/src/bitstring-status-list.ts
@@ -45,7 +45,6 @@ export class BitstringStatusList implements IAgentPlugin {
           did: args.bitstringStatusIssuer,
         })) as IIdentifier;
       } catch (e) {
-        console.error(e);
         throw new Error(
           `invalid_argument: credential.issuer must be a DID managed by this agent.`,
         );
