7adc4b95cb760829dd6d414740b09f24acbfa599
Nam Hoang <hoangxuannam160493@gmail.com>
2024-12-25 15:30:02 +0700

    feat: implement Swagger API for the new exposed APIs
    Signed-off-by: Nam Hoang <hoangxuannam160493@gmail.com>

--- packages/remote-server/src/api-router.ts
+++ packages/remote-server/src/api-router.ts
@@ -1,10 +1,16 @@
 import { IAgent } from '@vckit/core-types';
-import { Request, Response, NextFunction, Router, json } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { Request, Response, NextFunction, Router } from 'express';
 import Debug from 'debug';
+import swaggerDocument from './swagger/swagger.json' assert { type: 'json' };
+import { updateSwagger } from './swagger/helpers.js';
+import fs from 'fs';
 
 interface RequestWithAgent extends Request {
   agent?: IAgent;
 }
+
+const VERSION_FILE = 'version.json';
 
 const exposedMethods = [
   'didManagerGet',
@@ -36,8 +42,10 @@ export const ApiRouter = (): Router => {
   for (const exposedMethod of exposedMethods) {
     Debug('veramo:remote-server:initializing')(exposedMethod);
 
+    const apiVersion = getApiVersion();
+
     router.post(
-      '/' + exposedMethod,
+      `/${apiVersion}/${exposedMethod}`,
       async (req: RequestWithAgent, res: Response, next: NextFunction) => {
         if (!req.agent) throw Error('Agent not available');
         try {
@@ -60,5 +68,54 @@ export const ApiRouter = (): Router => {
       },
     );
   }
+
   return router;
+};
+
+export const SwaggerApiDocsRouter = (): Router => {
+  const router = Router();
+  const apiVersion = getApiVersion();
+
+  let swaggerJson: any = { ...swaggerDocument };
+
+  router.use(
+    '/',
+    (req: any, res: any, next: any) => {
+      const url = `${req.protocol}://${req.hostname}:${req.socket.localPort}/api/${apiVersion}`;
+      swaggerJson = updateSwagger(swaggerJson, { version: apiVersion, url });
+      req.swaggerDoc = swaggerJson;
+      next();
+    },
+    swaggerUi.serveFiles(swaggerJson, {}),
+    swaggerUi.setup(),
+  );
+
+  return router;
+};
+
+export const SwaggerApiSchemaRouter = (): Router => {
+  const router = Router();
+  const apiVersion = getApiVersion();
+
+  router.get('/', (req: RequestWithAgent, res) => {
+    if (req.agent) {
+      const url = `${req.protocol}://${req.hostname}:${req.socket.localPort}/api/${apiVersion}`;
+      swaggerDocument.servers = [{ url }] as any;
+      (swaggerDocument.info as any).version = apiVersion as string;
+
+      res.json(swaggerDocument);
+    } else {
+      res.status(500).json({ error: 'Agent not available' });
+    }
+  });
+
+  return router;
+};
+
+const getApiVersion = () => {
+  const version = fs.readFileSync(VERSION_FILE, 'utf8');
+  const { apiVersion } = JSON.parse(version);
+
+  if (!apiVersion) throw Error('API version not found');
+  return apiVersion;
 };
